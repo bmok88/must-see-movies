7ce1bd6b95ec6c7e66387e93d23ed4a0a76b6626
Brendan Mok <bmok88@gmail.com>
2019-11-09 16:20:17 -0800

    Add SearchBar to Movies

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Link } from '@reach/router';
+
+import SearchBar from './components/SearchBar';
 
 const App = () => {
-    return <div>owow</div>;
+    return (
+        <div>
+            <header>
+                <Link to="/">Must See Movies</Link>
+            </header>
+            <SearchBar />
+        </div>
+    );
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,9 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, FunctionComponent } from 'react';
+import { RouteComponentProps } from '@reach/router';
 
-const SearchBar = () => {
-    const [searchText, setSearchText] = useState('');
+const SearchBar: FunctionComponent<RouteComponentProps> = () => {
+    const [movie, setMovie] = useState('');
 
-    return <input type="text" />;
+    async function searchMovie() {}
+
+    return (
+        <form
+            onSubmit={e => {
+                e.preventDefault();
+                searchMovie();
+            }}
+        >
+            <input
+                type="text"
+                value={movie}
+                onChange={e => setMovie(e.target.value)}
+                onBlur={e => setMovie(e.target.value)}
+            />
+        </form>
+    );
 };
 
 export default SearchBar;
